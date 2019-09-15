35193177e99ef846592356f43bb5ca0dada72a7e
Ben Arundel <ben.arundel@cdl.co.uk>
2019-09-15 18:20:56 +0100

    clean up of example

--- aws/example/data.tf
+++ aws/example/data.tf
@@ -1,15 +1,15 @@
 data "aws_iam_account_alias" "current" {}
 data "aws_caller_identity" "current" {}
 
-data "aws_iam_policy_document" "cross_account_role_profile" {
+data "aws_iam_policy_document" "trust_profile" {
   statement {
-    sid = "AuthAssumeRole"
+    sid = "TrustProfile"
 
     principals {
       type = "AWS"
 
       identifiers = [
-        "arn:aws:iam::${var.assume_role_account_number}:root",
+        "arn:aws:iam::ACCOUNTNUMBER:root",
       ]
     }
 
@@ -30,69 +30,3 @@ data "aws_iam_policy_document" "iam_permissions" {
     resources = ["*"]
   }
 }
-
-data "aws_iam_policy_document" "iam_boundary" {
-
-  statement {
-    sid =       "IAMList"
-    actions =   [
-      "iam:ListServerCertificates",
-      "iam:ListPoliciesGrantingServiceAccess",
-      "iam:ListServiceSpecificCredentials",
-      "iam:ListMFADevices",
-      "iam:ListSigningCertificates",
-      "iam:ListVirtualMFADevices",
-      "iam:ListInstanceProfilesForRole",
-      "iam:ListSSHPublicKeys",
-      "iam:ListAttachedRolePolicies",
-      "iam:ListAttachedUserPolicies",
-      "iam:ListAttachedGroupPolicies",
-      "iam:ListRolePolicies",
-      "iam:ListAccessKeys",
-      "iam:ListPolicies",
-      "iam:ListSAMLProviders",
-      "iam:ListGroupPolicies",
-      "iam:ListEntitiesForPolicy",
-      "iam:ListRoles",
-      "iam:ListUserPolicies",
-      "iam:ListInstanceProfiles",
-      "iam:ListPolicyVersions",
-      "iam:ListOpenIDConnectProviders",
-      "iam:ListGroupsForUser",
-      "iam:ListAccountAliases",
-      "iam:ListUsers",
-      "iam:ListGroups",
-      "iam:GetLoginProfile",
-      "iam:GetAccountSummary",
-      "sts:GetCallerIdentity"
-    ]
-    effect =    "Allow"
-    resources = [
-      "*"]
-  }
-
-  statement {
-    sid =       "IAMRead"
-    actions =   [
-      "iam:GetPolicyVersion",
-      "iam:GetServiceLastAccessedDetailsWithEntities",
-      "iam:GenerateServiceLastAccessedDetails",
-      "iam:GetServiceLastAccessedDetails",
-      "iam:GetContextKeysForPrincipalPolicy",
-      "iam:GetServiceLinkedRoleDeletionStatus",
-      "iam:SimulateCustomPolicy",
-      "iam:SimulatePrincipalPolicy",
-      "iam:GetServerCertificate",
-      "iam:GetRole",
-      "iam:GetInstanceProfile",
-      "iam:GetPolicy",
-      "iam:GetContextKeysForCustomPolicy",
-      "iam:GetRolePolicy"
-    ]
-    effect =    "Allow"
-    resources = [
-      "*"]
-  }
-
-}
-
--- aws/example/main.tf
+++ aws/example/main.tf
@@ -1,15 +1,15 @@
 module "iam_example" {
   source = "../"
-  role_name                 = "AMRole"
+  role_name = "IAMRole"
 
   create_role = true
 
-  assume_role_policy        = "${data.aws_iam_policy_document.cross_account_role_profile.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.trust_profile.json}"
 
   inline_policies_to_create = {
     "AllowIAM" = "${data.aws_iam_policy_document.iam_permissions.json}"
   }
 
   inline_policy_count = 1
-
 }
+
