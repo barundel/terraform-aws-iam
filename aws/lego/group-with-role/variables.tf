77821e636d4685f232973feab3af70a1f6e2b824
Ben Arundel <ben.arundel@cdl.co.uk>
2019-11-29 11:08:17 +0000

    removing the type of a var

--- aws/lego/group-with-role/variables.tf
+++ aws/lego/group-with-role/variables.tf
@@ -1,2 +1,2 @@
 variable "group_name" {description = "Name of the group you want to create"}
-variable "policies_to_create" {type = map(string) description = "Policies you want to create for the group"}
+variable "policies_to_create" {description = "Policies you want to create for the group"}
